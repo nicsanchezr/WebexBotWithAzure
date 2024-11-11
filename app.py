144f171c1fc851a07be9810a57bccd7b643edd21
nicsanchezr <94387701+nicsanchezr@users.noreply.github.com>
2024-11-11 12:40:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from webex_bot.webex_bot import WebexBot
 from gpt import gpt
 
-bot = WebexBot("Your webex bot token")
+bot = WebexBot("NDlhNzQxNjktODk3My00MGJhLTg1MWQtZTAzMWRkMjc1Y2EwZDlhNzkyZDItNmE3_P0A1_edb06b55-c800-4c42-83d8-f014369a7fd3")
 bot.commands.clear()
 bot.add_command(gpt())
 bot.help_command = gpt()
